3e72c9a385875c7f663ae8e23901b99a247f6a4d
Jakub <jakubkrys@gmail.com>
2021-04-23 22:09:08 +0200

    #13 przygotowanie menu

---
+++ src/pl/jakubkrys/car_duel/Car.java
@@ -0,0 +1,14 @@
+package pl.jakubkrys.car_duel;
+
+public class Car {
+
+    private int price;
+    private double fuelConsumption;
+    private double engineCapacity;
+
+    public Car(int price, double fuelConsumption, double engineCapacity) {
+        this.price = price;
+        this.fuelConsumption = fuelConsumption;
+        this.engineCapacity = engineCapacity;
+    }
+}
---
+++ src/pl/jakubkrys/car_duel/CarDuelMain.java
@@ -0,0 +1,52 @@
+package pl.jakubkrys.car_duel;
+
+import java.util.Scanner;
+
+public class CarDuelMain {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int option;
+
+        do {
+
+            System.out.println("----- Car Duel -----");
+            System.out.println("1 - compare two cars");
+            System.out.println("0 - quit");
+            option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option){
+                case 1:
+                    System.out.println("--- Car 1 ---");
+                    System.out.print("Price: ");
+                    int price1 = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.print("Fuel consuming: ");
+                    double fuelConsuming1 = scanner.nextDouble();
+                    scanner.nextLine();
+                    System.out.print("Engine capacity: ");
+                    double engineCapacity1 = scanner.nextDouble();
+                    scanner.nextLine();
+
+                    System.out.println("--- Car 2 ---");
+                    System.out.print("Price: ");
+                    int price2 = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.print("Fuel consuming: ");
+                    double fuelConsuming2 = scanner.nextDouble();
+                    scanner.nextLine();
+                    System.out.print("Engine capacity: ");
+                    double engineCapacity2 = scanner.nextDouble();
+                    scanner.nextLine();
+
+                    Car car1 = new Car (price1,fuelConsuming1,engineCapacity1);
+                    Car car2 = new Car (price2,fuelConsuming2,engineCapacity2);
+                    break;
+                case 2:
+                    break;
+            }
+        } while (option != 0);
+        scanner.close();
+    }
+}
